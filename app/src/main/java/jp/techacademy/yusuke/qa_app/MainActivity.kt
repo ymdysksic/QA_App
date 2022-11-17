58c3e1f974996be7813e2b65ce9c9a5257e9b425
ymdysk <ymdysk@sic.shimano.co.jp>
2022-11-17 17:06:10 +0900

    Lesson8 Chap.5.5

--- app/src/main/java/jp/techacademy/yusuke/qa_app/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yusuke/qa_app/MainActivity.kt
@@ -1,21 +1,25 @@
 package jp.techacademy.yusuke.qa_app
 
-import android.content.Intent  // ← 追加
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import com.google.firebase.auth.FirebaseAuth  // ← 追加
+import androidx.appcompat.app.ActionBarDrawerToggle    // ← 追加
+import androidx.core.view.GravityCompat    // ← 追加
+import com.google.android.material.navigation.NavigationView    // ← 追加
+import com.google.firebase.auth.FirebaseAuth
 // findViewById()を呼び出さずに該当Viewを取得するために必要となるインポート宣言
-import kotlinx.android.synthetic.main.activity_main.*  // ← 追加
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*    // ← 追加
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {    // ← 修正
+
+    private var mGenre = 0    // ← 追加
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // ～～ ここから
         // idがtoolbarがインポート宣言により取得されているので
         // id名でActionBarのサポートを依頼
         setSupportActionBar(toolbar)
@@ -30,8 +34,17 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(applicationContext, LoginActivity::class.java)
                 startActivity(intent)
             }
-            // ～～ ここまで
         }
+
+        // ～～ ここから
+        // ナビゲーションドロワーの設定
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.app_name, R.string.app_name)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        // ～～ ここまで
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -49,4 +62,27 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    // ～～ ここから
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_hobby) {
+            toolbar.title = getString(R.string.menu_hobby_label)
+            mGenre = 1
+        } else if (id == R.id.nav_life) {
+            toolbar.title = getString(R.string.menu_life_label)
+            mGenre = 2
+        } else if (id == R.id.nav_health) {
+            toolbar.title = getString(R.string.menu_health_label)
+            mGenre = 3
+        } else if (id == R.id.nav_compter) {
+            toolbar.title = getString(R.string.menu_compter_label)
+            mGenre = 4
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    // ～～ ここまで
 }
